8824194e8e90fface9b795037d3a03982795f9a6
Javier Garcia <trad.ljgarcia@gmail.com>
2022-01-23 16:10:57 -0300

    desafio listo

--- Desafio1Garcia.js
+++ Desafio1Garcia.js
@@ -1,9 +1,10 @@
 class User {
-    constructor(name, surname, pets){
+    constructor(name, surname, pets, books){
             this.name = name;
             this.surname = surname;
             this.pets = pets;
             this.numberOfPets = 0;
+            this.books = books;
 
     }
 
@@ -21,15 +22,27 @@ class User {
 
     countPets(pets){
         this.numberOfPets += pets;
-    } 
+    }
+
+    getBooks(){
+        return this.books;
+    }
+
+    addBooks(newBooks){
+        this.books.push(newBooks);
+    }
+
 
 }
 
 
-const user1 = new User ('Javier', 'Garcia', ['lizard', 'bat'])
+const user1 = new User ('Javier', 'Garcia', ['lizard', 'bat'], ['The Shining, Stephen King'])
 console.log(user1)
+
 user1.getFullName()
 user1.addPets()
-user1.countPets(2)
-console.log(user1.numberOfPets)
+user1.countPets(3)
+console.log(`The number of pets I have is ${user1.numberOfPets}`)
+user1.addBooks('It, Stephen King');
+console.log(user1.getBooks())
 
