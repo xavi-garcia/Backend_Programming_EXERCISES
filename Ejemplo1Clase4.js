d4a68d5d2705f6784ba716e61c6d9412e0a0c3a1
Javier Garcia <trad.ljgarcia@gmail.com>
2022-01-27 21:36:51 -0300

    desafio 2

---
+++ Ejemplo1Clase4.js
@@ -0,0 +1,4 @@
+// Asincronismos y CallBacks
+// Desarrollar una función ‘mostrarLetras’ que reciba un string como parámetro y permita mostrar una vez por segundo cada uno de sus caracteres. 
+// Al finalizar, debe invocar a la siguiente función que se le pasa también  como parámetro: const fin = () => console.log('terminé')
+// Realizar tres llamadas a ‘mostrarLetras’ con el mensaje ‘¡Hola!’ y demoras de 0, 250 y 500 mS verificando que los mensajes de salida se intercalen.
